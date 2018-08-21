008a6efc079a3131e20454ed710d3f639476d9c4
MiKalec <msmikaelis@gmail.com>
2018-08-21 11:12:45 -0300

    Encapsulating the services of User, removing then from UserController and improving logs

--- src/main/java/br/com/carrental/control/UserController.java
+++ src/main/java/br/com/carrental/control/UserController.java
@@ -1,102 +1,37 @@
 package br.com.carrental.control;
 
 import br.com.carrental.dto.UserDTO;
-import br.com.carrental.model.User;
-import br.com.carrental.repository.UserRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import br.com.carrental.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 public class UserController {
     @Autowired
-    private UserRepository repository;
-
-    private final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+    private UserServiceImpl service;
 
     @GetMapping("/users")
     public ResponseEntity getUsers() {
-        List<User> list = repository.findAll();
-        List<UserDTO> listDTO = new ArrayList<>();
-        for (User user : list) {
-            listDTO.add(new UserDTO(user.getIdDocument(),
-                    user.getName(),
-                    user.getEmail(),
-                    user.getAddress(),
-                    new SimpleDateFormat("dd/MM/yyyy")
-                            .format(user.getBirthDate())));
-        }
-        LOGGER.info("Get all users success");
-        return new ResponseEntity(listDTO, HttpStatus.OK);
+        return service.getAllUsers();
     }
 
     @GetMapping("/users/{id}")
     public ResponseEntity getUser(@PathVariable Long id) {
-        Optional<User> user = repository.findById(id);
-
-        if (!user.isPresent()) {
-            LOGGER.warn("Get user id = {} not found, ERROR: 404", id);
-            return new ResponseEntity(HttpStatus.NOT_FOUND);
-        }
-
-        UserDTO userDTO = new UserDTO(user.get().getIdDocument(),
-                user.get().getName(),
-                user.get().getEmail(),
-                user.get().getAddress(),
-                new SimpleDateFormat("dd/MM/yyyy")
-                        .format(user.get().getBirthDate()));
-
-        LOGGER.info("Get user id = {} success", id);
-        return new ResponseEntity(userDTO, HttpStatus.OK);
+        return service.getUserById(id);
     }
 
     @DeleteMapping("/users/{id}")
     public ResponseEntity deleteUser(@PathVariable Long id) {
 
-        if (!repository.findById(id).isPresent()) {
-            LOGGER.warn("Delete user id = {} not found, ERROR:404", id);
-            return new ResponseEntity(HttpStatus.NOT_FOUND);
-        }
-        repository.deleteById(id);
-        LOGGER.info("Delete user id = {} success", id);
-        return new ResponseEntity(HttpStatus.OK);
+        return service.deleteUserById(id);
     }
 
     @PostMapping("/users")
     public ResponseEntity<Object> createUser(@RequestBody UserDTO user) throws ParseException {
-        User createdUser;
-        //verification if the user was already created
-        try {
-            createdUser = repository
-                    .save(new User(user.getIdDocument(),
-                    user.getName(),
-                    user.getEmail(),
-                    user.getAddress(),
-                    new SimpleDateFormat("dd/MM/yyyy")
-                            .parse(user.getBirthDate())));
-        } catch (DataIntegrityViolationException e) {
-            LOGGER.warn("Post user idDocument = {} has conflict, ERROR:409", user.getIdDocument());
-            return new ResponseEntity(HttpStatus.CONFLICT);
-        }
-
-        URI location = ServletUriComponentsBuilder
-                .fromCurrentRequest()
-                .path("/{id}")
-                .buildAndExpand(createdUser.getId())
-                .toUri();
-        LOGGER.info("Post user idDocument = {} success", createdUser.getIdDocument());
-        return ResponseEntity.created(location).build();
+        return service.addUser(user);
     }
+
 }
---
+++ src/main/java/br/com/carrental/service/UserServiceImpl.java
@@ -0,0 +1,101 @@
+package br.com.carrental.service;
+
+import br.com.carrental.control.UserController;
+import br.com.carrental.dto.UserDTO;
+import br.com.carrental.model.User;
+import br.com.carrental.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl {
+    @Autowired
+    private UserRepository repository;
+
+    private final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+
+    public ResponseEntity getAllUsers() {
+        List<User> list = repository.findAll();
+        List<UserDTO> listDTO = new ArrayList<>();
+        for (User user : list) {
+            listDTO.add(new UserDTO(user.getIdDocument(),
+                    user.getName(),
+                    user.getEmail(),
+                    user.getAddress(),
+                    new SimpleDateFormat("dd/MM/yyyy")
+                            .format(user.getBirthDate())));
+        }
+        LOGGER.info("m=getUsers: Get all users success");
+        return new ResponseEntity(listDTO, HttpStatus.OK);
+    }
+
+    public ResponseEntity getUserById(@PathVariable Long id) {
+        Optional<User> user = repository.findById(id);
+
+        if (!user.isPresent()) {
+            LOGGER.warn("m=getUser: Get user id = {} not found, ERROR: 404", id);
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+
+        UserDTO userDTO = new UserDTO(user.get().getIdDocument(),
+                user.get().getName(),
+                user.get().getEmail(),
+                user.get().getAddress(),
+                new SimpleDateFormat("dd/MM/yyyy")
+                        .format(user.get().getBirthDate()));
+
+        LOGGER.info("m=getUser: Get user id = {} success", id);
+        return new ResponseEntity(userDTO, HttpStatus.OK);
+    }
+
+    public ResponseEntity deleteUserById(@PathVariable Long id) {
+        if (!repository.findById(id).isPresent()) {
+            LOGGER.warn("m=deleteUser: Delete user id = {} not found, ERROR:404", id);
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+        repository.deleteById(id);
+        LOGGER.info("m=deleteUser: Delete user id = {} success", id);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    public ResponseEntity<Object> addUser(@RequestBody UserDTO user) throws ParseException {
+        User createdUser;
+        //verification if the user was already created
+        try {
+            createdUser = repository
+                    .save(new User(user.getIdDocument(),
+                            user.getName(),
+                            user.getEmail(),
+                            user.getAddress(),
+                            new SimpleDateFormat("dd/MM/yyyy")
+                                    .parse(user.getBirthDate())));
+        } catch (DataIntegrityViolationException e) {
+            LOGGER.warn("m=createUser: Post user idDocument = {} has conflict, ERROR:409", user.getIdDocument());
+            return new ResponseEntity(HttpStatus.CONFLICT);
+        }
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(createdUser.getId())
+                .toUri();
+        LOGGER.info("m=createUser: Post user idDocument = {} success", createdUser.getIdDocument());
+        return ResponseEntity.created(location).build();
+    }
+
+}
