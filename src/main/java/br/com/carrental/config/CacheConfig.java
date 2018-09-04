db281280c99289b90fe608e17b3d6ade8fd39d98
MiKalec <msmikaelis@gmail.com>
2018-09-04 10:33:02 -0300

    Adding JavaDoc to CacheConfig

--- src/main/java/br/com/carrental/config/CacheConfig.java
+++ src/main/java/br/com/carrental/config/CacheConfig.java
@@ -4,9 +4,9 @@ import com.google.common.cache.CacheBuilder;
 import java.util.concurrent.TimeUnit;
 import org.springframework.cache.Cache;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.guava.GuavaCache;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.cache.guava.GuavaCache;
 
 /**
  * Configuration class that enables caching
@@ -20,6 +20,11 @@ public class CacheConfig {
     public final static String CACHE_USERS = "users";
     public final static String CACHE_USER_ID = "usersById";
 
+    /**
+     * A Bean that creates the cache that will mantain the /users GET request. It will expires in 5 minutes after the write.
+     *
+     * @return Cache - Cache to mantain the users list.
+     */
     @Bean
     public Cache cacheUsers() {
         return new GuavaCache(CACHE_USERS, CacheBuilder
@@ -29,6 +34,11 @@ public class CacheConfig {
                 .build());
     }
 
+    /**
+     * A Bean that creates the cache that will mantain the /users/id GET request. It will expires in 5 minutes after the write.
+     *
+     * @return Cache - Cache to mantain the user with a given id.
+     */
     @Bean
     public Cache cacheUsersId() {
         return new GuavaCache(CACHE_USER_ID, CacheBuilder
