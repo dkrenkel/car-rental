55244e10bb76406dec52bbc157e4c058ca26b4af
MiKalec <msmikaelis@gmail.com>
2018-09-12 13:38:06 -0300

    Applying finals in method postOnMock

--- src/main/java/br/com/carrental/client/ClientRest.java
+++ src/main/java/br/com/carrental/client/ClientRest.java
@@ -15,8 +15,8 @@ public class ClientRest {
 
     private static final String URL_MOCK = "http://www.mocky.io/v2/5b97c2b42f000068007b3c59";
 
-    public void postOnMock(UserDTO user){
-        ResponseEntity<UserDTO> response = restTemplate.postForEntity(URL_MOCK, user, UserDTO.class);
+    public void postOnMock(final UserDTO user){
+        final ResponseEntity<UserDTO> response = restTemplate.postForEntity(URL_MOCK, user, UserDTO.class);
 
         LOGGER.info("m=postOnMock: Post on mock status: " +  response.getStatusCodeValue());
     }
